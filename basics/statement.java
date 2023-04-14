1517fb8454d3be88816408bccd5348e0ae38836e
pratik-karna <kaystha.pratik@gmail.com>
2023-04-14 13:01:57 +0530

    first commit

--- basics/statement.java
+++
@@ -1,9 +0,0 @@
-public class statement{  
-    public static void main(String[] args) {    
-        int age=20;   
-        if(age>18)
-        {  
-            System.out.print("Age is greater than 18");  
-        }  
-    }  
-}
--- basics/variable.java
+++
@@ -1,8 +0,0 @@
-public class variable {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 10;
-        int c = a + b;
-        System.out.println(c);
-    }
-}
