ccd753e4264c229a54fac5cfedc60ff06224c223
pratik-karna <kaystha.pratik@gmail.com>
2023-04-14 22:32:36 +0530

    first commit

---
+++ basics/number.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class number
+{
+   public static void main(String[] args)
+   {
+      int num=10;
+      System.out.println("\nThe Value of 'num' is " +num);
+   }
+}
