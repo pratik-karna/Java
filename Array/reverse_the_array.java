9c0b17ed95b39325a4376710e6ffaac495e51e79
pratik-karna <kaystha.pratik@gmail.com>
2023-05-11 22:44:10 +0530

    initial code of the reverse the array

---
+++ Array/reverse_the_array.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class reverse_the_array {
+    public static void main(String args[]) {
+        int counter, i = 0, j = 0, temp;
+        int number[] = new int[100];
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("How many elements you want to enter: ");
+        counter = scanner.nextInt();
+        for (i = 0; i < counter; i++) {
+            System.out.print("Enter Array Element" + (i + 1) + ": ");
+            number[i] = scanner.nextInt();
+        }
+        j = i - 1;
+        i = 0;
+        scanner.close();
+        while (i < j) {
+            temp = number[i];
+            number[i] = number[j];
+            number[j] = temp;
+            i++;
+            j--;
+        }
+
+        System.out.print("Reversed array: ");
+        for (i = 0; i < counter; i++) {
+            System.out.print(number[i] + "  ");
+        }
+    }
+}
